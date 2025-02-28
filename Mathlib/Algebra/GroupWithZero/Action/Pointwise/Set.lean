b68df99b9c39a5f1bb110386bbf544b4c3cf8bd2
Yaël Dillies <yael.dillies@gmail.com>
2025-02-28 11:41:08 +0000

    update copyright headers

--- Mathlib/Algebra/GroupWithZero/Action/Pointwise/Set.lean
+++ Mathlib/Algebra/GroupWithZero/Action/Pointwise/Set.lean
@@ -1,7 +1,7 @@
 /-
-Copyright (c) 2019 Johan Commelin. All rights reserved.
+Copyright (c) 2022 Yaël Dillies. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Johan Commelin, Floris van Doorn
+Authors: Yaël Dillies
 -/
 import Mathlib.Algebra.Group.Action.Pointwise.Set
 import Mathlib.Algebra.GroupWithZero.Action.Basic
